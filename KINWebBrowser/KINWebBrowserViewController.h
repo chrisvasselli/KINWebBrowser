31ddda094ed97a7d38c228bfd9fef72f34fe3ae6
Chris Vasselli <clindsay@gmail.com>
2021-03-18 11:07:22 -0400

    Remove another reference to UIWebView.

--- KINWebBrowser/KINWebBrowserViewController.h
+++ KINWebBrowser/KINWebBrowserViewController.h
@@ -68,7 +68,7 @@
  For convenience, two sets of static initializers are available.
  
  */
-@interface KINWebBrowserViewController : UIViewController <WKNavigationDelegate, WKUIDelegate, UIWebViewDelegate>
+@interface KINWebBrowserViewController : UIViewController <WKNavigationDelegate, WKUIDelegate>
 
 #pragma mark - Public Properties
 
